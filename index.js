2b5c0a5e8ca643ce8d8d53b0d126397bc90e30a5
izacpeterson <izacpeterson@gmail.com>
2022-05-06 14:53:12 -0600

    images

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
 app.use(express.static("public"));
 
 app.use("/scripts", express.static("scripts"));
+app.use("/images", express.static("images"));
 
 app.get("/update", (req, res) => {
   exec("./scripts/update.sh");
